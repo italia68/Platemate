08e1abda0b72d61611efc1813258fa6dbc0acdad
Itskua6868 <noto45@yahoo.com>
2026-06-15 13:23:54 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'platemate-v11';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-152x152.png',
+  './icon-167x167.png',
+  './icon-180x180.png',
+  './icon-192x192.png',
+  './icon-512x512.png'
+];
+
+// Install: cache all assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate: wipe ALL old caches immediately
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: network first, fall back to cache
+// This means deployed updates show up immediately
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Update cache with fresh response
+        var clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
